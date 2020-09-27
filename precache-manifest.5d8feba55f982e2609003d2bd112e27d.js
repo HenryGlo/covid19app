250971b8be4733617c013d80730acef55603d3bd
Henry Lofiego <henrylofiego@gmail.com>
2020-09-27 11:53:55 -0400

    Updates

---
+++ precache-manifest.5d8feba55f982e2609003d2bd112e27d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcaf48a5174e2fbaaf3dc79cb0c8640b",
+    "url": "/covid19app/index.html"
+  },
+  {
+    "revision": "3e4c7a04f89ca19a1fdf",
+    "url": "/covid19app/static/css/main.af17c795.chunk.css"
+  },
+  {
+    "revision": "d283f3896ab5340a900d",
+    "url": "/covid19app/static/js/2.a63ae349.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/covid19app/static/js/2.a63ae349.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e4c7a04f89ca19a1fdf",
+    "url": "/covid19app/static/js/main.96687ec0.chunk.js"
+  },
+  {
+    "revision": "50e70c769bbbe4efe4a9",
+    "url": "/covid19app/static/js/runtime-main.b432c4ec.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid19app/static/media/logo.5d5d9eef.svg"
+  }
+]);
